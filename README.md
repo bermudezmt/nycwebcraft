c40a629debd548c2271509ea54e5e184eafd311d
bermudezmt <bermudez.mt@gmail.com>
2016-03-14 22:24:03 -0700

    redoing repo

--- README.md
+++
@@ -1,5 +0,0 @@
-nycwebcraft
-===========
-
-My Portfolio Site
-
---
+++ wp-content/themes/webcraft/header.php
@@ -0,0 +1,66 @@
+ <!doctype html>
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Google Chrome Frame for IE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title><?php is_front_page() ? bloginfo('name') : wp_title(''); ?></title>
+
+		<!-- mobile meta (hooray!) -->
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+		<!-- icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) -->
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<!--[if IE]>
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<![endif]-->
+		<!-- or, set /favicon.ico for IE10 win -->
+		<meta name="msapplication-TileColor" content="#f01d4f">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<!-- wordpress head functions -->
+		<?php wp_head(); ?>
+		<!-- end of wordpress head -->
+
+		<!-- drop Google Analytics Here -->
+		<!-- end analytics -->
+
+	    <!-- ###: -->
+    
+    <!-- :### -->
+
+</head>
+
+	<body <?php body_class(); ?>>
+
+
+
+
+		<div id="container">
+
+			<header class="header" role="banner">
+
+				<div id="inner-header" class="wrap clearfix">
+
+					<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
+<a href="<?php echo home_url(); ?>" rel="nofollow"><img class="logo" src="/wp-content/themes/webcraft/library/images/header_bg_v2.png" title="NYC Webcraft for your website needs" alt="A web design and development team based in New York City"/></a>
+
+					<!-- if you'd like to use the site description you can un-comment it below -->
+				</div> <!-- end #inner-header -->
+					<?php // bloginfo('description'); ?>
+					<nav role="navigation">
+					<?php bones_main_nav(); ?>
+					</nav>
+			</header> <!-- end header -->
